728ebe494503195a81bd4cdbd574563b36729f27
LeoBarrios <leobarrios_23@hotmail.com>
2019-10-25 23:41:43 -0300

    ahora si funciona como debe

--- build.bat
+++ build.bat
@@ -11,4 +11,6 @@ mkdir ./bin/lib/
 g++ -Wall -std=c++11 -I./include/ main.cpp -o ./bin/main.bin
 
 ::compilo las bibliotecas
-g++ -shared -Wall -std=c++11 -I./include/ ./src/Test/Test.cpp -o ./bin/lib/Test.so
+g++ -shared -Wall -std=c++11 -I./include/ ./src/Test/Test.cpp -o ./bin/lib/Test.dll
+g++ -shared -Wall -std=c++11 -I./include/ ./src/Test2/Test2.cpp -o ./bin/lib/Test2.dll
+g++ -shared -Wall -std=c++11 -I./include/ ./src/Test3/Test3.cpp -o ./bin/lib/Test3.dll
